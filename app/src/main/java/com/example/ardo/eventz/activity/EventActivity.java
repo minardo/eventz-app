3cfa639a7e353edf159cf98e204ea4c2ab60cdc4
Muhamad Minardo <ardo15.project@gmail.com>
2018-06-20 16:09:25 +0700

    Get All Event

---
+++ app/src/main/java/com/example/ardo/eventz/activity/EventActivity.java
@@ -0,0 +1,15 @@
+package com.example.ardo.eventz.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ardo.eventz.R;
+
+public class EventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event);
+    }
+}
